a12386f744f9aee41c5835d15e5477fa0e72b399
Pkotova <petya.kotova@abv.bg>
2021-02-09 14:55:35 +0200

    fixed

--- Adrenaline/Scripts/script.js
+++ Adrenaline/Scripts/script.js
@@ -1,7 +1,24 @@
-'use strict';
+
 Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
     item.onclick = () => {
         item.parentElement.parentElement.classList.toggle("change");
     };
 });
 
+function validate() {
+    if( document.myForm.Name.value == "" ) {
+        alert( "Please provide your name!" );
+        document.myForm.Name.focus() ;
+        return false;
+    }
+    if( document.myForm.Email.value == "" ) {
+        alert( "Please provide your email!" );
+        document.myForm.Email.focus() ;
+        return false;
+    }
+    if( document.myForm.Message.value == "" ) {
+        alert( "Please provide your message!" );
+        document.myForm.Message.focus() ;
+        return false;
+    }
+}
--- Adrenaline/Scripts/style.js
+++ Adrenaline/Scripts/style.js
@@ -1,11 +1,14 @@
 'use strict';
+
 document.getElementById("center1").addEventListener("mouseover", mouseOver);
 document.getElementById("center1").addEventListener("mouseout", mouseOut);
 
 function mouseOver() {
     document.getElementById("center1").style.background = "grey";
+    console.log("hover")
 }
 
 function mouseOut() {
     document.getElementById("center1").style.background = "white";
+    console.log("hover")
 }
